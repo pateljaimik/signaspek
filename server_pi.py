05d00c5a3079be8cbb6c97c3c4ae1e62f22dfb07
raaiq <raufr@mcmaster.ca>
2024-03-11 00:49:49 -0400

    Added unix socket. (check if it can read multiple lines. Server doesn't exit properly)

--- server_pi.py
+++ server_pi.py
@@ -3,9 +3,12 @@ import asyncio
 import speech_recognition as sr
 import queue
 from aiortc import RTCIceCandidate, RTCPeerConnection, RTCSessionDescription
+from ctypes import * 
 import sys
+import os
 
 audio_queue = queue.Queue()
+text_queue = asyncio.Queue()
 sampling_rate= 16000
 def channel_log(channel, t, message):
     print("channel(%s) %s %s" % (channel.label, t, message))
@@ -53,13 +56,58 @@ async def server(pc, signaling):
 
     @channel.on("message")
     def on_message(message):
-        print(message)
+        print(f"Recieved message:{message}")
+        try:
+            text_queue.put_nowait(message)
+        except:
+            print("Text queue is full")
+
     
     await consume_signaling(pc, signaling)
 
+# Creating Unix socket to send text data to OLED display application
 
-    
+
+async def unix_socket_callback(reader, writer):
+
+    while True:
+        text= await text_queue.get()
+        print("Writing to socket")
+        writer.write((text+"\n").encode())
+        await writer.drain()
+
+async def unix_soc_server():
+    socket_path="/tmp/SignaSpek"
+    try:
+        os.unlink(socket_path)
+    except OSError:
+        if os.path.exists(socket_path):
+            raise
+    soc_server= await asyncio.start_unix_server(client_connected_cb=unix_socket_callback,path=socket_path)
+    print("Unix socket intialized\n")
+    async with soc_server:
+        await soc_server.serve_forever() 
+    print("Socket server closed")
+
+async def mainAsync(pc,signaling):
+    a=asyncio.create_task(unix_soc_server())
+    b= asyncio.create_task(server(pc,signaling))
+    await asyncio.gather(a,b)
+
+# Code to get rid of JACK errors on startup
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
+asound = cdll.LoadLibrary("libasound.so.2")
+
+
+
 if __name__ == "__main__":
+        asound.snd_lib_error_set_handler(c_error_handler)
         recorder = sr.Recognizer()
         recorder.energy_threshold = 1000
         recorder.dynamic_energy_threshold = False
@@ -68,7 +116,7 @@ if __name__ == "__main__":
 
         with source:
             recorder.adjust_for_ambient_noise(source)
-        
+
         def audio_callback(_, audio:sr.AudioData):
             data = audio.get_raw_data()
             audio_queue.put(data)
@@ -76,11 +124,12 @@ if __name__ == "__main__":
         recorder.listen_in_background(source, audio_callback, phrase_time_limit=2)
         print("Microphone initialized")
 
+        loop = asyncio.get_event_loop()
         signaling = WebsocketSignaling("raspberrypi.local","8765")
         pc = RTCPeerConnection()
-        coro = server(pc, signaling)
+        coro = mainAsync(pc, signaling)
 
-        loop = asyncio.get_event_loop()
+        
         try:
             loop.run_until_complete(coro)
         except KeyboardInterrupt:
@@ -88,3 +137,5 @@ if __name__ == "__main__":
         finally: 
             loop.run_until_complete(pc.close())
             loop.run_until_complete(signaling.close())
+
+        asound.snd_lib_error_set_handler(None)
